1379c01ba11f93cbde9a77f346908b80f0c1e547
Mohamed Taher Alrefaie <m.taher@msn.com>
2018-03-28 11:37:10 +0100

    changed TICC to a class with `predict` method.

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
-import TICC_solver as TICC
+from TICC_solver import TICC
 import numpy as np
 import sys
 
 fname = "example_data.txt"
-(cluster_assignment, cluster_MRFs) = TICC.solve(window_size = 1,number_of_clusters = 8, lambda_parameter = 11e-2, beta = 600, maxIters = 100, threshold = 2e-5, write_out_file = False, input_file = fname, prefix_string = "output_folder/", num_proc=1)
+ticc = TICC(window_size=1, number_of_clusters=8, lambda_parameter=11e-2, beta=600, maxIters=100, threshold=2e-5,
+            write_out_file=False, prefix_string="output_folder/", num_proc=1)
+(cluster_assignment, cluster_MRFs) = ticc.fit(input_file=fname)
 
 print cluster_assignment
 np.savetxt('Results.txt', cluster_assignment, fmt='%d', delimiter=',')
